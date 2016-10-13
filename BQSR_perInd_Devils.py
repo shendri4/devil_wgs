efa00333573eeae6c248b64d55d0c20cd7a1ca3d
shendri4 <shendri4@gmail.com>
2016-10-13 15:16:09 -0700

    modify BQSR.py script

--- BQSR_perInd_Devils.py
+++ BQSR_perInd_Devils.py
@@ -15,7 +15,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', "--samples", help="Samples.txt file with sample ID.", required=True)
 parser.add_argument('-b', "--bwaindex", help="Path to bwa index file.", required=True)
-parser.add_argument('-k', "--knownsites", help="path and fileName of filteredSNP.vcf.", required=True)
+parser.add_argument('-k', "--knownsites", help="Path and fileName of filteredSNP.vcf.", required=True)
 args = parser.parse_args()
 #args = parser.parse_args('-s samples.txt -r /mnt/lfs2/hend6746/fox_cancer/0rawdata_test -b /mnt/lfs2/hend6746/wolves/reference/canfam31/canfam31.fa'.split())
 
@@ -37,7 +37,7 @@ for l in open(args.samples):
 # Setup folders and paths variables:
 bamFolder = abspath('02-Mapped')
 variantFolder = abspath('03-Calls')
-PBS_scripts = abspath('GATK_PBS_scripts')
+PBS_scripts = abspath('BQSR_PBS_scripts')
 #rawdataDir = abspath(args.rawdata)
 bwaIndex = abspath(args.bwaindex)
 knownSites = abspath(args.knownsites)
@@ -51,8 +51,8 @@ os.system('mkdir -p %s' % PBS_scripts)
 for sample in samples:
     print "Processing", sample, "....."
     # Set up files:
-    logFile = jp(variantFolder, sample + '_GATK.log')
-    logCommands = open(jp(PBS_scripts, sample + '_commands.sh'), 'w')
+    logFile = jp(bamFolder, sample + '_BQSR.log')
+    logCommands = open(jp(PBS_scripts, sample + '_BQSR_commands.sh'), 'w')
 
     #Setup for qsub
     log('#!/bin/bash', logCommands)
@@ -69,13 +69,11 @@ for sample in samples:
 
 ####################
 #	BaseQualityRecalibration
-#	Step 1: first run of BQSR: BaseRecalibrator
+#	Step 1: First run of BQSR: BaseRecalibrator
 ####################
-    cmd = ' '.join([gatkCall, ' -nct 24 ',
-    ' -T BaseRecalibrator ',
-    + jp(bamFolder, sample) + '.bam',
+    cmd = ' '.join([gatkCall, ' -nct 24 ', ' -T BaseRecalibrator ', 'I' + jp(bamFolder, sample) + '.bam',
     ' -knownSites ' knownSites,
-    ' -o ' + jp(variantFolder, sample) + '.table', '>>', logFile, '2>&1'])
+    ' -o ' + jp(bamFolder, sample) + '_BQSR.table', '>>', logFile, '2>&1'])
     log(cmd, logCommands)
 
 ####################
@@ -83,38 +81,26 @@ for sample in samples:
 #	Step 2: BaseRecalibrator on recalibrated files
 ####################
 # 
-# for CURRENT_KEY in `cat ${INPUT_DIR}/big_output/bam/${KEY}_addRG_realign_fixMate.list`
-# do
-# java -Xmx120g -jar /mnt/home/hend6746/modules/GenomeAnalysisTK-2.6-4-g3e5ff60/GenomeAnalysisTK.jar \
-# 	-nct 24 \
-# 	-T BaseRecalibrator \
-# 	-I ${WORK_DIR}/${CURRENT_KEY} \
-# 	-R ${PROJ_DIR}/reference/canfam31/canfam31.fa \
-# 	-knownSites ${PROJ_DIR}/vcf/union_variants_max100000_min2var_variants.vcf \
-# 	-BQSR recal_data_${CURRENT_KEY}_min2var_FIXED.table \
-# 	-o post_recal_data_${CURRENT_KEY}_min2var_FIXED.table \
-# 	--intervals ${PROJ_DIR}/bed_files/baits_canfam3.1_sorted_merged.bed \
-# 	--interval_padding 1000
-# done
+#     cmd = ' '.join([gatkCall, ' -nct 24 ',
+#     ' -T BaseRecalibrator ',
+#     'I' + jp(bamFolder, sample) + '.bam',
+#     ' -knownSites ' knownSites,
+#     ' -BQSR ' + jp(bamFolder, sample) + '_BQSR.table'
+#     ' -o ' + jp(bamFolder, sample) + '_BQSR_FIXED.table', '>>', logFile, '2>&1'])
+#     log(cmd, logCommands)
 # 
 # ####################
 # #	BaseQualityRecalibration
 # #	Step 3: PrintReads
-# # 	Apply recalibration table to original bam file
+# #	Apply recalibration table to original bam file
 # ####################
 # 
-# for CURRENT_KEY in `cat ${INPUT_DIR}/big_output/bam/${KEY}_addRG_realign_fixMate.list`
-# do
-# java -Xmx120g -jar /mnt/home/hend6746/modules/GenomeAnalysisTK-2.6-4-g3e5ff60/GenomeAnalysisTK.jar \
-# 	-nct 24 \
-#     -T PrintReads \
-# 	-I ${WORK_DIR}/${CURRENT_KEY} \
-#     -R ${PROJ_DIR}/reference/canfam31/canfam31.fa \
-#     -BQSR recal_data_${CURRENT_KEY}_min2var_FIXED.table \
-# 	-o recal_${CURRENT_KEY}_min2var_FIXED.bam \
-# 	--intervals ${PROJ_DIR}/bed_files/baits_canfam3.1_sorted_merged.bed \
-# 	--interval_padding 1000
-# done
-# exit 0;
-    
+#     cmd = ' '.join([gatkCall, ' -nct 24 ',
+#     ' -T PrintReads ',
+#     'I' + jp(bamFolder, sample) + '.bam',
+#     ' -knownSites ' knownSites,
+#     ' -BQSR ' + jp(bamFolder, sample) + '_BQSR_FIXED.table'
+#     ' -o ' + jp(bamFolder, sample) + '_BQSR_FIXED.bam', '>>', logFile, '2>&1'])
+#     log(cmd, logCommands)
+
     logCommands.close()
