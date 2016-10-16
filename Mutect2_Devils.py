2f5b6b7e05ea9359ff19c44b680534b340be8ba8
shendri4 <shendri4@gmail.com>
2016-10-16 10:34:44 -0700

    test #1

--- Mutect2_Devils.py
+++ Mutect2_Devils.py
@@ -64,9 +64,9 @@ for normalsample in normalsamples:
 
     #Setup for qsub
     log('#!/bin/bash', logCommands)
-    log('#PBS -N %s' % tumorsample, logCommands)
+    log('#PBS -N %s' % tumorsamples, logCommands)
     log('#PBS -j oe', logCommands)
-    log('#PBS -o %s_job.log' % tumorsample, logCommands)
+    log('#PBS -o %s_job.log' % tumorsamples, logCommands)
     log('#PBS -m abe', logCommands)
     log('#PBS -M shendri4@gmail.com', logCommands)
     log('#PBS -q short', logCommands)
