d5a90ba33e562f98267a2351bacf4cc8259f8ad1
shendri4 <shendri4@gmail.com>
2016-10-16 16:10:27 -0700

    clarify comments on input files

--- Mutect2_tumor_Devils.py
+++ Mutect2_tumor_Devils.py
@@ -2,9 +2,9 @@
 #import argparse
 #from glob import glob
 
-#-s test_samples.txt
+#-s test_normalsamples.txt
+#-s test_tumarsamples.txt
 #-b /mnt/lfs2/hend6746/devils/reference/sarHar1.fa
-#-k /mnt/lfs2/hend6746/taz/filtered_plink_files/export_data_150907/seventy.1-2.nodoubletons.noparalogs.noX.plink.oneperlocus.vcf
 
 from os.path import join as jp
 from os.path import abspath
