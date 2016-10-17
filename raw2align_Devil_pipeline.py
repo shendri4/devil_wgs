9b6acb572aba791bdfa1f5d5a8b8e6d4d5ed0ddc
shendri4 <shendri4@gmail.com>
2016-10-17 13:37:46 -0700

    fixed path

--- raw2align_Devil_pipeline.py
+++ raw2align_Devil_pipeline.py
@@ -101,7 +101,7 @@ for sample in samples:
     # the --max-overlap was set to 600, but that seems really long; default is 65, try 400
     # --max-overlap 400 --min-overlap 15 --max-mismatch-density .10 --min-overlap-outie 35 --percent-cutoff 25
     cmd = ' '.join(['flash2 --max-overlap 150 --allow-outies --threads 7 -o', jp(resultsDir, sample + '_flash'),
-                    '-d', resultsDir, jp(rawdataDir, sample + '.fastq.1.gz'), jp(rawdataDir, sample + '.fastq.2.gz'),
+                    '-d', jp(rawdataDir, sample + '.fastq.1.gz'), jp(rawdataDir, sample + '.fastq.2.gz'),
                     '>>', logFile, '2>&1'])
     log(cmd, logCommands)
 
