0905f882c6dc8b121cf8c188242b427dba71acef
shendri4 <shendri4@gmail.com>
2016-10-17 16:12:25 -0700

    fix bug in picard markdups command

--- raw2align_Devil_pipeline.py
+++ raw2align_Devil_pipeline.py
@@ -166,8 +166,8 @@ for sample in samples:
 #     os.system(cmd)
 # 
 # Mark PCR duplicates (remove duplicates, if desired)
-    cmd = ' '.jp([picardCall, ' INPUT=' + jp(bamFolder, sample + ".bam"), ' OUTPUT=' + jp(bamFolder, sample + "markdup.bam"),
-    ' METRICS_FILE=' + jp(bamFolder, sample + ".metrics"), ' REMOVE_DUPLICATES=false ',
+    cmd = ' '.jp([picardCall, ' INPUT=', jp(bamFolder, sample + ".bam"), ' OUTPUT=', jp(bamFolder, sample + "markdup.bam"),
+    ' METRICS_FILE=', jp(bamFolder, sample + ".metrics"), ' REMOVE_DUPLICATES=false ',
     ' ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT TMP_DIR=/temp'])
     log(cmd, logCommands)
 
